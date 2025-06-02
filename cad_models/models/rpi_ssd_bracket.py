571fae3743d228d3ae579e8a2bc2dba94ae2549f
Ben Fiola <benfiola@gmail.com>
2025-06-02 11:42:46 -0700

    adjust rpi ssd bracket dimensions, add magnet arms

--- cad_models/models/rpi_ssd_bracket.py
+++ cad_models/models/rpi_ssd_bracket.py
@@ -4,6 +4,7 @@ from build123d import (
     Axis,
     BuildPart,
     BuildSketch,
+    Circle,
     GridLocations,
     Location,
     Locations,
@@ -13,6 +14,8 @@ from build123d import (
     Rectangle,
     Vector,
     extrude,
+    fillet,
+    mirror,
 )
 
 from cad_models.common import CaptiveNutSlot, Iso, Model, main
@@ -44,26 +47,33 @@ class BracketNut(HexNut):
 class RpiSsdBracket(Model):
     def __init__(self, **kwargs):
         # parameters
+        corner_radius = 2 * MM
+        dimensions = Vector(70 * MM, 100 * MM)
+        magnet_arm_dimensions = Vector(10 * MM, 10 * MM)
+        magnet_dimensions = Vector(6.5 * MM, 3 * MM)
         nut = BracketNut()
         rpi_standoff_dimensions = Vector(8.5 * MM, 8.5 * MM, 5 * MM)
         rpi_standoff_offset = 5.5 * MM
         rpi_standoff_spacing = Vector(49 * MM, 58 * MM)
         screw = BracketScrew()
-        ssd_dimensions = Vector(70 * MM, 100 * MM)
         ssd_hole_spacing = Vector(62.0 * MM, 77.0 * MM)
         ssd_hole_offset = 2.5 * MM
-        thickness = 4 * MM
+        thickness = 5 * MM
 
         with BuildPart() as builder:
+            fillet_edges = []
+
             # create ssd bracket
             with BuildSketch(Plane.XZ):
-                Rectangle(ssd_dimensions.X, thickness)
-            extrude(amount=ssd_dimensions.Y)
+                Rectangle(dimensions.X, thickness)
+            solid = extrude(amount=dimensions.Y)
+            edges = solid.edges().filter_by(Axis.Z)
+            fillet_edges.extend(edges)
 
             # create ssd bracket holes
             face = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[1]
             with BuildSketch(face, mode=Mode.PRIVATE):
-                location = Location((0, ssd_hole_offset))
+                location = Location((0, -ssd_hole_offset))
                 with Locations(location):
                     with GridLocations(
                         ssd_hole_spacing.X, ssd_hole_spacing.Y, 2, 2
@@ -75,7 +85,7 @@ class RpiSsdBracket(Model):
             # create raspberry pi standoffs
             face = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-1]
             with BuildSketch(face):
-                location = Location((0, rpi_standoff_offset))
+                location = Location((0, -rpi_standoff_offset))
                 with Locations(location):
                     with GridLocations(
                         rpi_standoff_spacing.X, rpi_standoff_spacing.Y, 2, 2
@@ -94,6 +104,28 @@ class RpiSsdBracket(Model):
             locations = [l * Pos(Z=-nut_offset) for l in locations]
             with Locations(*locations):
                 CaptiveNutSlot(nut, width=rpi_standoff_dimensions.X)
+
+            # create magnet arms
+            face = builder.faces().filter_by(Axis.X).sort_by(Axis.X)[0]
+            with BuildSketch(face):
+                Rectangle(thickness, magnet_arm_dimensions.Y)
+            mirror_plane = Plane(face.offset(-dimensions.X / 2))
+            solid = magnet_arm = extrude(amount=magnet_arm_dimensions.X)
+            edges = solid.edges().filter_by(Axis.Z).sort_by(Axis.X)[:2]
+            fillet_edges.extend(edges)
+            solid = mirror(magnet_arm, mirror_plane)
+            edges = solid.edges().filter_by(Axis.Z).sort_by(Axis.X)[-2:]
+            fillet_edges.extend(edges)
+
+            # create magnet holes
+            face = magnet_arm.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
+            with BuildSketch(face):
+                Circle(magnet_dimensions.X / 2)
+            solid = extrude(amount=-magnet_dimensions.Y, mode=Mode.SUBTRACT)
+            mirror(solid, mirror_plane, mode=Mode.SUBTRACT)
+
+            # create fillet
+            fillet(fillet_edges, corner_radius)
 
         super().__init__(builder.part, **kwargs)
 
