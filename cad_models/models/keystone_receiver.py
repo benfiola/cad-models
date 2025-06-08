687a0622d09354826705df4a856f017182b8af41
Ben Fiola <benfiola@gmail.com>
2025-06-08 09:38:11 -0700

    remove taper from keystone receiver

--- cad_models/models/keystone_receiver.py
+++ cad_models/models/keystone_receiver.py
@@ -1,6 +1,22 @@
 from typing import cast
 
-from build123d import Axis, Location, RigidJoint, Rot, Solid, Vector, import_step
+from build123d import (
+    MM,
+    Align,
+    Axis,
+    BuildPart,
+    BuildSketch,
+    Location,
+    Pos,
+    Rectangle,
+    RigidJoint,
+    Rot,
+    Solid,
+    Vector,
+    add,
+    extrude,
+    import_step,
+)
 
 from cad_models.common import Model, main
 from cad_models.data import data_file
@@ -13,16 +29,28 @@ class KeystoneReceiver(Model):
         solid: Solid = cast(Solid, import_step(data_file("keystone-receiver.step")))
         solid = solid.rotate(Axis.X, 90)
 
-        # find faces to calculate size + joint location
-        front_face = solid.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
-        bottom_faces = solid.faces().filter_by(Axis.Z).sort_by(Axis.Z)[:3]
+        with BuildPart() as builder:
+            add(solid)
 
-        # create joint
-        joint_location = Location(front_face.location_at(0.5, 0.5))
-        joint_location *= Rot(Z=90)
-        RigidJoint("keystone", to_part=solid, joint_location=joint_location)
+            # find faces to calculate size + joint location
+            front_face = builder.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
+            bottom_faces = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[:3]
 
-        super().__init__(solid, **kwargs)
+            # fix top of receiver to no longer taper to a point
+            amendment = Vector(14.9 * MM, 1.7 * MM, 3.08 * MM)
+            location = front_face.location_at(0.5, 1.0)
+            location.orientation = front_face.orientation
+            location *= Pos(Z=-6.150)
+            with BuildSketch(location):
+                Rectangle(amendment.X, amendment.Y, align=(Align.CENTER, Align.MIN))
+            extrude(amount=amendment.Z)
+
+            # create joint
+            joint_location = Location(front_face.location_at(0.5, 0.5))
+            joint_location *= Rot(Z=90)
+            RigidJoint("keystone", to_part=builder.part, joint_location=joint_location)
+
+            super().__init__(builder.part, **kwargs)
 
         # calculate dimensions from faces
         width = 0.0
