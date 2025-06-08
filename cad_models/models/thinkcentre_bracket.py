4bc04a184ff88d782454230d71d38e7da952e90f
Ben Fiola <benfiola@gmail.com>
2025-06-08 00:34:16 -0700

    implement power supply tray thinkcentre bracket

--- cad_models/models/thinkcentre_bracket.py
+++ cad_models/models/thinkcentre_bracket.py
@@ -2,18 +2,24 @@ from copy import copy
 
 from build123d import (
     MM,
+    Align,
     Axis,
     BuildPart,
     BuildSketch,
+    Circle,
     GridLocations,
     Location,
+    Locations,
     Mode,
+    Plane,
+    Pos,
     Rectangle,
     RigidJoint,
     Rot,
     Vector,
     add,
     extrude,
+    mirror,
 )
 
 from cad_models.common import Model, ServerRackMountBracket, U, main
@@ -35,6 +41,12 @@ class ThinkcentreBracket(Model):
             base_keystone = KeystoneReceiver()
         keystone_spacing = 25 * MM
         interface_holes = Vector(3, 2)
+        cable_diameter = 4.1 * MM
+        cable_slot_width = 3.8 * MM
+        cable_tray_dimensions = Vector(20 * MM, 10 * MM, 70 * MM)
+        power_supply_tray_dimensions = Vector(65 * MM, 6 * MM, 145 * MM)
+        tray_offset = 12.5 * MM
+        tray_thickness = 2 * MM
 
         with BuildPart() as builder:
             # create bracket
@@ -69,10 +81,108 @@ class ThinkcentreBracket(Model):
                     add(keystone)
 
             if power_supply_tray:
-                pass
+                # find inner corner of rib
+                face = builder.faces().filter_by(Axis.Z).group_by(Axis.Z)[-3][0]
+                edge = face.edges().filter_by(Axis.X)[0]
+                base_location = edge.location_at(0.0)
+                base_location.orientation = face.orientation
 
+                # create tray bases
+                with BuildSketch(base_location):
+                    # power supply
+                    width = power_supply_tray_dimensions.X + tray_thickness
+                    height = (
+                        tray_offset
+                        + tray_thickness
+                        + power_supply_tray_dimensions.Z
+                        + tray_thickness
+                    )
+                    Rectangle(width, height, align=Align.MIN)
+
+                    location = Location((0, 0))
+                    location *= Pos(X=power_supply_tray_dimensions.X)
+                    location *= Pos(X=tray_thickness)
+                    with Locations(location):
+                        width = cable_tray_dimensions.X + tray_thickness
+                        height = (
+                            tray_offset
+                            + tray_thickness
+                            + cable_tray_dimensions.Z
+                            + tray_thickness
+                        )
+                        Rectangle(width, height, align=Align.MIN)
+                extrude(amount=-face_thickness)
+
+                # create power supply tray solid
+                with BuildSketch(base_location):
+                    location = Location((0, 0))
+                    location *= Pos(Y=tray_offset)
+                    with Locations(location):
+                        width = power_supply_tray_dimensions.X + tray_thickness
+                        height = (
+                            tray_thickness
+                            + power_supply_tray_dimensions.Z
+                            + tray_thickness
+                        )
+                        Rectangle(width, height, align=Align.MIN)
+                solid = extrude(amount=power_supply_tray_dimensions.Y)
+
+                # create power supply tray cutout
+                face = solid.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
+                with BuildSketch(face):
+                    location = Location((-face.length / 2, -face.width / 2))
+                    location *= Pos(Y=tray_thickness)
+                    with Locations(location) as locs:
+                        Rectangle(
+                            power_supply_tray_dimensions.X,
+                            power_supply_tray_dimensions.Z,
+                            align=(Align.MIN, Align.MIN),
+                        )
+                extrude(amount=-power_supply_tray_dimensions.Y, mode=Mode.SUBTRACT)
+
+                # create cable tray solid
+                with BuildSketch(base_location):
+                    location = Location((0, 0))
+                    location *= Pos(X=power_supply_tray_dimensions.X)
+                    location *= Pos(X=tray_thickness)
+                    location *= Pos(Y=tray_offset)
+                    with Locations(location):
+                        width = cable_tray_dimensions.X + tray_thickness
+                        height = (
+                            tray_thickness + cable_tray_dimensions.Z + tray_thickness
+                        )
+                        Rectangle(width, height, align=Align.MIN)
+                solid = extrude(amount=cable_tray_dimensions.Y)
+
+                # create cable tray cutout
+                face = solid.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
+                with BuildSketch(face):
+                    location = Location((-face.length / 2, -face.width / 2))
+                    location *= Pos(Y=tray_thickness)
+                    with Locations(location) as locs:
+                        Rectangle(
+                            cable_tray_dimensions.X,
+                            cable_tray_dimensions.Z,
+                            align=(Align.MIN, Align.MIN),
+                        )
+                extrude(amount=-cable_tray_dimensions.Y, mode=Mode.SUBTRACT)
+
+                # create cable tray cable slots
+                face = solid.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
+                with BuildSketch(face):
+                    Circle(cable_diameter / 2)
+                    Rectangle(
+                        cable_slot_width,
+                        cable_tray_dimensions.Y,
+                        align=(Align.CENTER, Align.MIN),
+                    )
+                solid = extrude(amount=-tray_thickness, mode=Mode.SUBTRACT)
+                mirror_plane = Plane(face).offset(
+                    -((cable_tray_dimensions.Z / 2) + tray_thickness)
+                )
+                mirror(solid, mirror_plane, mode=Mode.SUBTRACT)
         super().__init__(builder.part, **kwargs)
 
 
 if __name__ == "__main__":
-    main(ThinkcentreBracket())
+    main(ThinkcentreBracket(keystone_receivers=False, power_supply_tray=True))
