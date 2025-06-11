72a539f9750d42ee60afda33c875d268f7f991df
Ben Fiola <benfiola@gmail.com>
2025-06-11 07:45:12 -0700

    adjust test model to purely focus on cable slots

--- cad_models/models/test.py
+++ cad_models/models/test.py
@@ -5,11 +5,8 @@ from build123d import (
     BuildPart,
     BuildSketch,
     Circle,
-    Location,
-    Locations,
     Mode,
     Plane,
-    Pos,
     Rectangle,
     Vector,
     extrude,
@@ -24,89 +21,30 @@ class Test(Model):
         # parameters
         face_thickness = 4 * MM
         cable_diameter = 4.3 * MM
-        cable_slot_width = 3.9 * MM
-        cable_tray_dimensions = Vector(31.5 * MM, 10 * MM, 119 * MM)
-        cable_tray_offset = 25.0 * MM
-        power_supply_tray_dimensions = Vector(64 * MM, 4 * MM, 144 * MM)
+        cable_slot_width = 3.8 * MM
+        cable_tray_dimensions = Vector(15 * MM, 10 * MM, 50 * MM)
         tray_thickness = 2 * MM
 
         with BuildPart() as builder:
             # create tray bases
             base_location = Plane.XY.location
             with BuildSketch(base_location):
-                # power supply
-                width = tray_thickness + power_supply_tray_dimensions.X + tray_thickness
-                height = (
-                    tray_thickness + power_supply_tray_dimensions.Z + tray_thickness
-                )
-                Rectangle(width, height, align=Align.MIN)
-
-                location = Location((0, 0))
-                location *= Pos(X=tray_thickness)
-                location *= Pos(X=power_supply_tray_dimensions.X)
-                location *= Pos(X=tray_thickness)
-                with Locations(location):
-                    width = cable_tray_dimensions.X + tray_thickness
-                    height = (
-                        cable_tray_offset
-                        + tray_thickness
-                        + cable_tray_dimensions.Z
-                        + tray_thickness
-                    )
-                    Rectangle(width, height, align=Align.MIN)
+                width = cable_tray_dimensions.X + tray_thickness
+                height = tray_thickness + cable_tray_dimensions.Z + tray_thickness
+                Rectangle(width, height)
             extrude(amount=-face_thickness)
-
-            # create power supply tray solid
-            with BuildSketch(base_location):
-                location = Location((0, 0))
-                with Locations(location):
-                    width = (
-                        tray_thickness + power_supply_tray_dimensions.X + tray_thickness
-                    )
-                    height = (
-                        tray_thickness + power_supply_tray_dimensions.Z + tray_thickness
-                    )
-                    Rectangle(width, height, align=Align.MIN)
-            solid = extrude(amount=power_supply_tray_dimensions.Y)
-
-            # create power supply tray cutout
-            face = solid.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
-            with BuildSketch(face):
-                location = Location((-face.length / 2, -face.width / 2))
-                location *= Pos(X=tray_thickness)
-                location *= Pos(Y=tray_thickness)
-                with Locations(location) as locs:
-                    Rectangle(
-                        power_supply_tray_dimensions.X,
-                        power_supply_tray_dimensions.Z,
-                        align=(Align.MIN, Align.MIN),
-                    )
-            extrude(amount=-power_supply_tray_dimensions.Y, mode=Mode.SUBTRACT)
 
             # create cable tray solid
             with BuildSketch(base_location):
-                location = Location((0, 0))
-                location *= Pos(X=tray_thickness)
-                location *= Pos(X=power_supply_tray_dimensions.X)
-                location *= Pos(X=tray_thickness)
-                location *= Pos(Y=cable_tray_offset)
-                with Locations(location):
-                    width = cable_tray_dimensions.X + tray_thickness
-                    height = tray_thickness + cable_tray_dimensions.Z + tray_thickness
-                    Rectangle(width, height, align=Align.MIN)
+                width = cable_tray_dimensions.X + tray_thickness
+                height = tray_thickness + cable_tray_dimensions.Z + tray_thickness
+                Rectangle(width, height)
             solid = extrude(amount=cable_tray_dimensions.Y)
 
             # create cable tray cutout
             face = solid.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
             with BuildSketch(face):
-                location = Location((-face.length / 2, -face.width / 2))
-                location *= Pos(Y=tray_thickness)
-                with Locations(location) as locs:
-                    Rectangle(
-                        cable_tray_dimensions.X,
-                        cable_tray_dimensions.Z,
-                        align=(Align.MIN, Align.MIN),
-                    )
+                Rectangle(cable_tray_dimensions.X, cable_tray_dimensions.Z)
             extrude(amount=-cable_tray_dimensions.Y, mode=Mode.SUBTRACT)
 
             # create cable tray cable slots
