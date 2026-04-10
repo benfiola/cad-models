6e7d38b11407c8033aa9f450ef5e878811a3db49
Ben Fiola <benfiola@gmail.com>
2026-04-10 16:56:13 -0700

    fix some things

--- cad_models/common.py
+++ cad_models/common.py
@@ -7,22 +7,34 @@ from build123d import BuildPart, Compound, export_step
 from ocp_vscode.show import show_all
 
 
+def set_label(build_part: BuildPart, label: str):
+    if not build_part.part:
+        raise ValueError("inner part is None")
+    build_part.part.label = label
+
+
 @dataclass
 class Args:
     export: pathlib.Path | None
     ocp: bool = False
 
 
-def main(*parts: BuildPart):
+def main(*build_parts: BuildPart):
     parser = argparse.ArgumentParser()
     parser.add_argument("--ocp", default=False, action="store_true")
     parser.add_argument("--export", default=None, type=pathlib.Path)
     args = Args(**vars(parser.parse_args(sys.argv[1:])))
 
     if args.ocp:
-        show_all()
+        variables = {}
+        for build_part in build_parts:
+            if not build_part.part:
+                continue
+            variables[build_part.part.label] = build_part
+        show_all(variables=variables)
+
     if args.export:
-        shape = Compound(parts)
+        shape = Compound(build_parts)
         if args.export.suffix == ".step":
             export_step(shape, args.export)
         else:
--- cad_models/keystone_surface_mount.py
+++ cad_models/keystone_surface_mount.py
@@ -1,10 +1,16 @@
 from build123d import *
-from common import main
+from common import *
 
-with BuildPart() as part:
+with BuildPart() as plate:
     with BuildSketch():
         Rectangle(1.0, 1.0)
     extrude(amount=1.0)
+set_label(plate, "Plate")
 
+with BuildPart() as cover:
+    with BuildSketch():
+        Rectangle(2.0, 2.0)
+    extrude(amount=0.5)
+set_label(cover, "Cover")
 
-main(part)
+main(plate, cover)
