4437b62c08eb37867f3fe31989372d093732ced1
Ben Fiola <bfiola@apple.com>
2026-04-13 18:54:37 -0700

    still idk

--- cad_models/common.py
+++ cad_models/common.py
@@ -1,8 +1,4 @@
-import argparse
 import copy
-import pathlib
-import sys
-from dataclasses import dataclass
 from typing import ClassVar
 
 from build123d import (
@@ -10,28 +6,43 @@ from build123d import (
     Axis,
     BasePartObject,
     BuildPart,
-    Compound,
     Location,
     Mode,
     Part,
     RigidJoint,
+<<<<<<< Updated upstream
     Rotation,
+=======
+    RotationLike,
+>>>>>>> Stashed changes
     Solid,
-    export_step,
     import_step,
 )
+<<<<<<< Updated upstream
 from ocp_vscode.show import show_object
+=======
+from build123d.topology.utils import tuplify
+>>>>>>> Stashed changes
 
 from cad_models.data import get_data_file
 
 
 def ensure_part(part: Part | None) -> Part:
+<<<<<<< Updated upstream
     if part is None:
         raise ValueError(f"part is none")
+=======
+    if not part:
+        raise ValueError("part is None")
+>>>>>>> Stashed changes
     return part
 
 
 class KeystoneReceiver(BasePartObject):
+<<<<<<< Updated upstream
+=======
+    _solid: ClassVar[Solid | None] = None
+>>>>>>> Stashed changes
     _applies_to = [BuildPart._tag]
     _solid: ClassVar[Solid | None] = None
 
@@ -42,6 +53,7 @@ class KeystoneReceiver(BasePartObject):
             solid = import_step(step_file)
             if not isinstance(solid, Solid):
                 raise TypeError(solid)
+<<<<<<< Updated upstream
             face = solid.faces().sort_by(Axis.Z)[-1]
             joint_location = Location(face.center_location)
             RigidJoint("joint", solid, joint_location)
@@ -66,6 +78,29 @@ class KeystoneReceiver(BasePartObject):
             solid = cls.get_solid()
         return solid.bounding_box().size.X
 
+=======
+            face = solid.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
+            joint_location = Location(face.location_at(0.5, 0.5).position, (0, 0, 0))
+            RigidJoint("joint", solid, joint_location)
+            cls._solid = solid
+        return copy.copy(cls._solid)
+
+    @classmethod
+    def depth(cls) -> float:
+        with BuildPart(mode=Mode.PRIVATE):
+            return cls.get_solid().bounding_box().size.Y
+
+    @classmethod
+    def height(cls) -> float:
+        with BuildPart(mode=Mode.PRIVATE):
+            return cls.get_solid().bounding_box().size.Z
+
+    @classmethod
+    def width(cls) -> float:
+        with BuildPart(mode=Mode.PRIVATE):
+            return cls.get_solid().bounding_box().size.X
+
+>>>>>>> Stashed changes
     def __init__(
         self,
         rotation: Rotation | tuple[float, float, float] = (0, 0, 0),
@@ -77,6 +112,7 @@ class KeystoneReceiver(BasePartObject):
         mode: Mode = Mode.ADD,
     ):
         solid = self.get_solid()
+<<<<<<< Updated upstream
         super().__init__(solid, rotation, align, mode)
 
 
@@ -109,3 +145,6 @@ def main(compound: Compound):
             export_step(compound, args.export)
         else:
             raise ValueError(f"unknown export extension: {args.export}")
+=======
+        super().__init__(solid, rotation, tuplify(align, 3), mode)
+>>>>>>> Stashed changes
--- cad_models/keystone_surface_mount.py
+++ cad_models/keystone_surface_mount.py
@@ -1,18 +1,38 @@
 import typing
 
+<<<<<<< Updated upstream
 from bd_warehouse import *
+=======
+>>>>>>> Stashed changes
 from build123d import *
 from common import *
+from ocp_vscode.show import show_object
 
+<<<<<<< Updated upstream
 cover_dovetail_depth = 3 * MM
 insert_height = 76.2 * MM
 insert_width = 44.45 * MM
 keystone_count = 5
 keystone_vertical_spacing = 3 * MM
+=======
+box_width = 150 * MM
+box_depth = 50 * MM
+box_height = 150 * MM
+cover_dovetail_depth = 3 * MM
+cover_fillet_radius = 1 * MM
+insert_height = 76.2 * MM
+insert_width = 44.45 * MM
+keystone_left_count = 4
+keystone_right_count = 4
+keystone_bottom_count = 1
+keystone_top_count = 0
+keystone_spacing = 3 * MM
+>>>>>>> Stashed changes
 screw_radius = 2.0 * MM
 screw_head_radius = 3.0 * MM
 screw_vertical_spacing = 96.8 * MM
 wall_thickness_x = KeystoneReceiver.depth()
+<<<<<<< Updated upstream
 wall_thickness_y = 5 * MM
 wall_thickness_z = 5 * MM
 depth = 50 * MM
@@ -120,3 +140,111 @@ box = MountBox()
 box.label = "Box"
 compound = Compound(children=[box, cover])
 main(compound)
+=======
+wall_thickness_y = KeystoneReceiver.depth()
+wall_thickness_z = 5 * MM
+
+with BuildPart() as cover_builder:
+    # cover
+    inner_height = box_height - (wall_thickness_y)
+    inner_width = box_width - (wall_thickness_x * 2)
+    with BuildSketch():
+        with BuildLine():
+            Polyline(
+                (0, 0),
+                (cover_dovetail_depth + inner_width + cover_dovetail_depth, 0),
+                (cover_dovetail_depth + inner_width, -wall_thickness_z),
+                (cover_dovetail_depth, -wall_thickness_z),
+                (0, 0),
+            )
+        make_face()
+    extrude(amount=inner_height)
+
+    # fillet
+    edges = cover_builder.edges().filter_by(Axis.Z).sort_by(Axis.X)
+    edges = [edges[0], edges[-1]]
+    fillet(edges, cover_fillet_radius)
+
+    # joint
+    face = cover_builder.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
+    joint_location = Location(face.location_at(0.5, 0.0).position, (0, 0, 0))
+    RigidJoint("joint", cover_builder.part, joint_location)
+
+with BuildPart() as box_builder:
+    # box
+    with BuildSketch(Plane.XZ):
+        Rectangle(box_width, box_height)
+    extrude(amount=box_depth)
+
+    # shell
+    face = box_builder.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0]
+    inner_depth = box_depth - (wall_thickness_z)
+    inner_height = box_height - (wall_thickness_y * 2)
+    inner_width = box_width - (wall_thickness_x * 2)
+    with BuildSketch(face):
+        Rectangle(inner_width, inner_height)
+    extrude(amount=-inner_depth, mode=Mode.SUBTRACT)
+
+    # cover slot
+    face = box_builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-1]
+    cover = copy.copy(ensure_part(cover_builder.part))
+    joint_location = Location(face.location_at(0.5, 0.0).position, (0, 0, 0))
+    joint = RigidJoint("cover", box_builder.part, joint_location)
+    joint.connect_to(typing.cast(RigidJoint, cover.joints["joint"]))
+    add(cover, mode=Mode.SUBTRACT)
+
+    # insert hole
+    face = box_builder.faces().filter_by(Axis.Y).sort_by(Axis.Y)[-2]
+    with BuildSketch(face):
+        Rectangle(insert_width, insert_height)
+    extrude(amount=-wall_thickness_z, mode=Mode.SUBTRACT)
+
+    # screw holes
+    face = box_builder.faces().filter_by(Axis.Y).sort_by(Axis.Y)[-2]
+    with Locations(face):
+        with GridLocations(0, screw_vertical_spacing, 1, 2):
+            CounterSinkHole(screw_radius, screw_head_radius, wall_thickness_z)
+
+    # keystone receiver cutouts
+    spacing = KeystoneReceiver.height() + keystone_spacing
+    keystone_locations: list[Location] = []
+    if keystone_left_count:
+        face = box_builder.faces().filter_by(Axis.X).sort_by(Axis.X)[0]
+        with BuildSketch(Plane(face, x_dir=(0, -1, 0))):
+            with GridLocations(0, spacing, 1, keystone_left_count) as grid_locs:
+                Rectangle(KeystoneReceiver.width(), KeystoneReceiver.height())
+                keystone_locations += grid_locs.locations
+        extrude(amount=-wall_thickness_x, mode=Mode.SUBTRACT)
+    if keystone_right_count:
+        face = box_builder.faces().filter_by(Axis.X).sort_by(Axis.X)[-1]
+        with BuildSketch(Plane(face, x_dir=(0, 1, 0))):
+            with GridLocations(0, spacing, 1, keystone_right_count) as grid_locs:
+                Rectangle(KeystoneReceiver.width(), KeystoneReceiver.height())
+                keystone_locations += grid_locs.locations
+        extrude(amount=-wall_thickness_x, mode=Mode.SUBTRACT)
+    if keystone_bottom_count:
+        face = box_builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
+        with BuildSketch(Plane(face, x_dir=(0, -1, 0))):
+            with GridLocations(0, spacing, 1, keystone_bottom_count) as grid_locs:
+                Rectangle(KeystoneReceiver.width(), KeystoneReceiver.height())
+                keystone_locations += grid_locs.locations
+        extrude(amount=-wall_thickness_y, mode=Mode.SUBTRACT)
+    if keystone_top_count:
+        face = box_builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-1]
+        with BuildSketch(Plane(face, x_dir=(0, 1, 0))):
+            with GridLocations(0, spacing, 1, keystone_top_count) as grid_locs:
+                Rectangle(KeystoneReceiver.width(), KeystoneReceiver.height())
+                keystone_locations += grid_locs.locations
+        extrude(amount=-wall_thickness_y, mode=Mode.SUBTRACT)
+
+    # keystone receivers
+    for index, keystone_location in enumerate(keystone_locations):
+        receiver = KeystoneReceiver()
+        receiver_joint = typing.cast(RigidJoint, receiver.joints["joint"])
+        joint_location = Location(keystone_location) * Rot(X=-90)
+        joint = RigidJoint(f"keystone-{index}", box_builder.part, joint_location)
+        joint.connect_to(receiver_joint)
+        print("here")
+
+show_object(box_builder.part)
+>>>>>>> Stashed changes
