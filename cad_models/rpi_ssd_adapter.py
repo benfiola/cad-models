dff4c408a7be993debfc3e29b3bf2a445fbf92cc
Ben Fiola <benfiola@gmail.com>
2026-05-07 15:17:25 -0700

    progress on rpi ssd bracket

--- cad_models/rpi_ssd_adapter.py
+++ cad_models/rpi_ssd_adapter.py
@@ -1,3 +1,4 @@
+from bd_warehouse.fastener import *
 from build123d import *
 from common import *
 
@@ -11,12 +12,20 @@ class Parameters:
     ssd_spacing_x: float = 62.0 * MM
     ssd_spacing_y: float = 77.0 * MM
     ssd_offset_y: float = -2.5 * MM
+    ssd_screw_size: str = "M3-0.5"
     standoff_spacing_x: float = 49 * MM
     standoff_spacing_y: float = 58 * MM
     standoff_offset_y: float = -5.5 * MM
+    standoff_diameter: float = 5 * MM
+    standoff_nut_size: str = "M3-0.5-Standard"
+    standoff_nut_depth: float = 3 * MM
 
 
 def builder_fn(p: Parameters):
+    with BuildPart(mode=Mode.PRIVATE):
+        screw = PanHeadScrew(p.ssd_screw_size, p.adapter_thickness)
+        nut = HeatSetNut(p.standoff_nut_size)
+
     with BuildPart() as builder:
         # adapter
         with BuildSketch():
@@ -24,22 +33,24 @@ def builder_fn(p: Parameters):
         extrude(amount=p.adapter_thickness)
 
         # ssd holes
-        face = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[0]
-        plane = Plane(face.position, x_dir=(1, 0, 0), z_dir=(face.normal_at()))
+        face = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-1]
+        plane = Plane(face.center(), x_dir=(1, 0, 0), z_dir=(face.normal_at()))
         location = Location(plane)
         location *= Pos(Y=p.ssd_offset_y)
-        with Locations(Plane(face, x_dir=(1, 0, 0))):
+        with Locations(location):
             with GridLocations(p.ssd_spacing_x, p.ssd_spacing_y, 2, 2):
-                Cylinder(2, 1, mode=Mode.SUBTRACT)
+                ClearanceHole(screw)
 
         # standoff holes
         face = builder.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-1]
-        plane = Plane(face.position, x_dir=(1, 0, 0), z_dir=face.normal_at())
+        plane = Plane(
+            face.without_holes().center(), x_dir=(1, 0, 0), z_dir=face.normal_at()
+        )
         location = Location(plane)
         location *= Pos(Y=p.standoff_offset_y)
         with Locations(location):
             with GridLocations(p.standoff_spacing_x, p.standoff_spacing_y, 2, 2):
-                Cylinder(2, 1, mode=Mode.SUBTRACT)
+                InsertHole(nut, depth=p.standoff_nut_depth)
 
         # fillet
         edges = builder.edges().filter_by(Axis.Z)
